a5925f89271cd3b911913df5604e864070c522a8
idan <idanov@post.bgu.ac.il>
2022-05-08 22:43:24 +0300

    add token verification - test

--- users-service/src/helpers/verifyToken.js
+++ users-service/src/helpers/verifyToken.js
@@ -30,4 +30,6 @@ const verifyTokenAndAdmin = (req, res , next) => {
 };
 
 
-module.exports = { verifyToken , verifyTokenAndAuthorization , verifyTokenAndAdmin };
+module.exports = { 
+    verifyToken , verifyTokenAndAuthorization , verifyTokenAndAdmin
+};
